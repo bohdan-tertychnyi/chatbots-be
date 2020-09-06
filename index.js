be89a185ae00240a28ca6a5ad8f10c171aff0543
Bohdan Tertychnyi <bogdan.dev.w@gmail.com>
2020-09-06 03:43:25 +0300

    Add sockets support

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('express')()
 const cors = require('cors')
+
 app.use(cors())
+
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const port = 3337
@@ -9,8 +11,29 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+// const connections = []
+
 io.on('connection', (socket) => {
-    console.log('user connected')
+    // connections.push(socket)
+    // socket.on('disconnect', data => {
+    //   console.log(data)
+    // })
+
+    socket.on('request-bot', data => {
+      const {botId, message} = data
+      // switch (botId) {
+      //   case value:
+          
+      //     break;
+      
+      //   default:
+      //     break;
+      // }
+
+      console.log('OPERATING WITH DB ...')
+
+      socket.emit('response-bot', {message: Math.round(Math.random() * 100)})
+    })
 })
 
 http.listen(port, () => {
